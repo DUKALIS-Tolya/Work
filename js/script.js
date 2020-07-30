a4694f281eaa26a14a871052143449fd9ec2cee7
Evgeny <zh.goncharov97@gmail.com>
2020-07-30 06:57:49 +0300

    Lesson 15, homework, final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-"use strict";
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-// console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,74 +10,27 @@ const personalMovieDB = {
     private: false
 };
 
-if(personalMovieDB.count < 10){
-    alert('Просмотрено довольно мало фильмов');
-}else if(personalMovieDB.count <=30){
-    alert('Вы классический зритель');
-}else{
-    alert('Произошла ошибка');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+    
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
 
-// console.log(personalMovieDB.count);
-
-/* const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; */
-
-for (let i = 0; i < 2; i++){
-    let a = prompt('Один из последних просмотренных фильмов?', '');
-    if (a == '' || a == null  || a.length > 50){
-        i--;
-        continue;
-    }
-
-    let b = prompt('На сколько оцените его?', '');
-    if (b == '' || b == null  || b.length > 50){
-        i--;
-        continue;
-    }
-
-    personalMovieDB.movies[a] = b;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
-
-/* let i = 0;
-while (i < 2){
-    i++;
-    let a = prompt('Один из последних просмотренных фильмов?', '');
-    if (a == '' || a == null  || a.length > 50){
-        i--;
-        continue;
-    }
-
-    let b = prompt('На сколько оцените его?', '');
-    if (b == '' || b == null  || b.length > 50){
-        i--;
-        continue;
-    }
-
-    personalMovieDB.movies[a] = b;
-} */
-
-/* let i = 0;
-do{
-    i++;
-    let a = prompt('Один из последних просмотренных фильмов?', '');
-    if (a == '' || a == null  || a.length > 50){
-        i--;
-        continue;
-    }
-
-    let b = prompt('На сколько оцените его?', '');
-    if (b == '' || b == null  || b.length > 50){
-        i--;
-        continue;
-    }
-
-    personalMovieDB.movies[a] = b;
-}while (i < 2); */
 
 console.log(personalMovieDB);

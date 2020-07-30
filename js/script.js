d46868f3c0eff89f8c3e486bcf31f90a5e728cc6
Evgeny <zh.goncharov97@gmail.com>
2020-07-30 04:31:59 +0300

    Lesson 15, homework, point 4

--- js/script.js
+++ js/script.js
@@ -46,4 +46,40 @@ for (let i = 0; i < 2; i++){
     personalMovieDB.movies[a] = b;
 }
 
+/* let i = 0;
+while (i < 2){
+    i++;
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    if (a == '' || a == null  || a.length > 50){
+        i--;
+        continue;
+    }
+
+    let b = prompt('На сколько оцените его?', '');
+    if (b == '' || b == null  || b.length > 50){
+        i--;
+        continue;
+    }
+
+    personalMovieDB.movies[a] = b;
+} */
+
+/* let i = 0;
+do{
+    i++;
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    if (a == '' || a == null  || a.length > 50){
+        i--;
+        continue;
+    }
+
+    let b = prompt('На сколько оцените его?', '');
+    if (b == '' || b == null  || b.length > 50){
+        i--;
+        continue;
+    }
+
+    personalMovieDB.movies[a] = b;
+}while (i < 2); */
+
 console.log(personalMovieDB);

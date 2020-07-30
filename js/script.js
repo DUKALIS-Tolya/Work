d359d55460b7af268597378a35a603d351543464
Evgeny <zh.goncharov97@gmail.com>
2020-07-30 04:05:26 +0300

    Lesson 15, homework, point 2

--- js/script.js
+++ js/script.js
@@ -23,8 +23,18 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d; */
 
 for (let i = 0; i < 2; i++){
-    let a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    if (a == '' || a == null  || a.length > 50){
+        i--;
+        continue;
+    }
+
+    let b = prompt('На сколько оцените его?', '');
+    if (b == '' || b == null  || b.length > 50){
+        i--;
+        continue;
+    }
+
     personalMovieDB.movies[a] = b;
 }
 
